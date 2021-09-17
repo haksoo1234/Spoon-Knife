f1fc24e57603dd04228d50063269f6c98ac9041c
Haksoo Kim <75501945+haksoo1234@users.noreply.github.com>
2021-09-16 20:23:57 -0700

    Update BitArrayUtility.h

--- BitArrayUtility.h
+++ BitArrayUtility.h
@@ -3,8 +3,6 @@
 #include <string>
 #include <string_view>
 
-namespace ZXing {
-
 class BitArray;
 
 namespace Utility {
@@ -13,4 +11,4 @@ namespace Utility {
 	std::string ToString(const BitArray& BitArray);
 	BitArray ParseBitArray(std::string_view str, char one = 'X');
 
-}} // ZXing::Utility
+}} 
